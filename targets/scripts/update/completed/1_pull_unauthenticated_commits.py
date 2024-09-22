e7bba6732f56a8178f67d601356d6824b0b0c106
n-dusan <nikolic.dusan.dey@gmail.com>
2024-09-22 04:02:25 +0000

    Sign targets and metadata

---
+++ targets/scripts/update/completed/1_pull_unauthenticated_commits.py
@@ -0,0 +1,33 @@
+import sys
+import json
+
+from pathlib import Path
+from taf.git import GitRepository
+from taf.log import taf_logger
+
+LIB_ROOT_PATH = Path(__file__).parent.parent.parent.parent.parent.parent.parent.expanduser()
+# LIB_ROOT_PATH = Path("/home/dnikolic/narf")
+
+# TODO: make more flexible
+repos_with_unauthenticated_commits = ("law-html",)
+
+def process_stdin():
+   return sys.stdin.read()
+
+def send_state(state):
+    print(json.dumps(state))
+
+data = process_stdin()
+data = json.loads(data)
+
+for repo in repos_with_unauthenticated_commits:
+    html_repo = GitRepository(library_dir=LIB_ROOT_PATH, name=f"narf-nill/{repo}")
+    error = None
+    try:
+        html_repo.pull()
+    except Exception as e:
+        send_state({"error": str(e)})
+        taf_logger.error(f"Pull failed for {repo.name}: {e}")
+        sys.exit(1)
+
+send_state(data)
