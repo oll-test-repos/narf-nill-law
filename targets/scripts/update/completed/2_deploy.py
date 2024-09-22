3e6fc03e2bf1f208c1e02a1cbca697355755bb2f
n-dusan <nikolic.dusan.dey@gmail.com>
2024-09-22 03:54:08 +0000

    Sign targets and metadata

--- targets/scripts/update/completed/2_deploy.py
+++ targets/scripts/update/completed/2_deploy.py
@@ -24,6 +24,13 @@ data = json.loads(data)
 # TODO: make html repo initialization more flexible
 html_repo = GitRepository(library_dir=LIB_ROOT_PATH, name="narf-nill/law-html")
 error = None
+
+try:
+    # pull current branch because law-html allows unauthenticated commits
+    html_repo.pull()
+except Exception as e:
+    send_state({"error": str(e)})
+    sys.exit(1)
 try:
     html_repo.commit(MESSAGE_TEMPLATE)
 except NothingToCommitError as e:
