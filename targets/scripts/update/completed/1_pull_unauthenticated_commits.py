19e11251fc24ba4972d9fa35459112713097a315
n-dusan <nikolic.dusan.dey@gmail.com>
2024-10-17 21:24:33 +0000

    Sign targets and metadata

--- targets/scripts/update/completed/1_pull_unauthenticated_commits.py
+++ targets/scripts/update/completed/1_pull_unauthenticated_commits.py
@@ -19,13 +19,13 @@ data = process_stdin()
 data = json.loads(data)
 
 for repo in target_repos_with_unauthenticated_commits:
-    html_repo = GitRepository(library_dir=LIB_ROOT_PATH, name=repo)
+    html_repo = GitRepository(library_dir=LIB_ROOT_PATH, name=repo, urls=["git@github.com:narf-nill/law-html.git"])
     error = None
     try:
-        html_repo.pull()
+        html_repo.clone()
     except Exception as e:
         send_state({"error": str(e)})
-        taf_logger.error(f"Pull failed for {repo.name}: {e}")
+        taf_logger.error(f"Clone failed for {repo.name}: {e}")
         sys.exit(1)
 
 send_state(data)
